466a8b017d0cc9583fe787e75480d3558b852963
Nauroze Hoath <naurozehoath@gmail.com>
2019-11-16 14:21:56 -0500

    Solution to 1.4

---
+++ 1_ArrayandStrings/4_Palindrome_Permutation.py
@@ -0,0 +1,30 @@
+# Given a string, write a function to check if it is a permutation of a palindrome. 
+# A palindrome is a word or phrase that is the same forwards and backwards. 
+# A permutation is a rearrangement of letters. The palindrome does not need to be limited to just dictionary words.
+
+
+# if there are at lease two odd numbers, then it is not a palindrome permutation
+# it can only be a palindrome permutation with 1 or less odd numbers
+def isAtleastTwoOdds(array):
+    oddcount = 0
+    for i in array:
+        if (i%2 != 0):
+            oddcount += 1
+
+        if (oddcount > 1):
+            return True
+    
+    return False
+
+def isPalindromePermutation(string):
+    string = string.lower()
+    letters = [0]*200
+
+    for c in string:
+        if (c != " "):
+            letters[ord(c)] += 1
+
+    if (isAtleastTwoOdds(letters)):
+        return False
+    else:
+        return True
---
+++ 1_ArrayandStrings/5_One_Away.py
@@ -0,0 +1,8 @@
+def one_away(string1, string1):
+    return is_one_additional(string1, string2) && is_one_less(string1, string2) && is_one_replaced(string1, string2)
+
+def is_one_additional(string1, string2):
+    if ( abs( len(string1) - len(string2) ) == 1 ):
+        
+    else:
+        return False
