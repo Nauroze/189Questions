65b6ea693330b7d52e35372cbf8819c5520b4c0f
Nauroze Hoath <naurozehoath@gmail.com>
2019-11-17 15:15:09 -0500

    Solution for 1.5

--- 1_ArrayandStrings/5_One_Away.py
+++ 1_ArrayandStrings/5_One_Away.py
@@ -1,8 +1,32 @@
-def one_away(string1, string1):
-    return is_one_additional(string1, string2) && is_one_less(string1, string2) && is_one_replaced(string1, string2)
+def OneEditReplace(s1, s2):
+    found_difference = False
+    for i in range(0, len(s1)):
+        if (s1[i] != s2[i]):
+            if (found_difference):
+                return False
+            found_difference = True
+    return True
 
-def is_one_additional(string1, string2):
-    if ( abs( len(string1) - len(string2) ) == 1 ):
-        
-    else:
-        return False
+def OneEditInsert(s1, s2):
+    index1 = 0
+    index2 = 0
+    while index2 < len(s2) and index1 < len(s1):
+        if s1[index1] != s2[index2]:
+            if (index1 != index2):
+                return False
+            index2 += 1
+        else:
+            index1 += 1
+            index2 += 1
+    return True
+
+def OneAway(s1, s2):
+    if len(s1) == len(s2):
+        return OneEditReplace(s1, s2)
+    elif (len(s1)+1) == len(s2):
+        return OneEditInsert(s1, s2)
+    elif (len(s1)-1) == len(s2):
+        return OneEditInsert(s2, s1)
+    return False
+
+print (OneAway("pales", "paled"))
